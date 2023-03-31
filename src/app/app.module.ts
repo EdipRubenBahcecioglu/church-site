c22d7bf196558ad06d34e0b297273f33ccb1da17
EdipRubenBahcecioglu <edip-bahcecioglu@hotmail.de>
2023-03-31 10:27:14 +0200

    changed secondsfor slideshow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ImageDiashowComponent } from './image-diashow/image-diashow.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ImageDiashowComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
